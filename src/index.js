414f4428c90025f64cbd3c8b4ed8b7467f2bded2
dburgos26 <d.burgos@uniandes.edu.co>
2023-09-26 09:28:53 -0500

    Génesis 1:1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const App = () => {
+  const language = navigator.language.split(/[-_]/)[0]; // language without region code
+  const messages = language === "en" ? localeEnMessages : localeEsMessages;
+
+  return (
+    <IntlProvider locale={language} messages={messages}>
+      <JobsList />
+    </IntlProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
